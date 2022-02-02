5665df6d62d847132aabb164b1462ac5e2054b6d
Kasim Mahroof <kasim@kasimmahroof.com>
2022-02-02 21:56:43 +0000

    Install Turbo

--- stubs/default/resources/js/app.js
+++ stubs/default/resources/js/app.js
@@ -1,5 +1,6 @@
 require('./bootstrap');
 
+import * as Turbo from "@hotwired/turbo"
 import { Application } from "@hotwired/stimulus";
 import { definitionsFromContext } from "@hotwired/stimulus-webpack-helpers";
 import Dropdown from "stimulus-dropdown";
